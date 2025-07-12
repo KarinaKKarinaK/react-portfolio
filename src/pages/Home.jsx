ad026e2b33e6741f71af3351f4a1641fac2a391f
KarinaKKarinaK <karinakalicka@gmail.com>
2025-07-13 01:06:52 +0400

    Adding and updating teh Toogle theme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = () => {
-    return <div className="min-h-screen">
-        {/* Theme Toggle*/}
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+            {/* Theme Toggle*/}
+            <ThemeToggle />
 
-        {/* Background EFfects */}
+            {/* Background EFfects */}
 
-        {/* Navbar */}
+            {/* Navbar */}
 
-        {/* Main Content */}
+            {/* Main Content */}
 
-        {/* Footer */}
-    </div>;
+            {/* Footer */}
+        </div>
+    );
 };
