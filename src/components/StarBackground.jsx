a6a65a153e3b656bfc1cee0733d45c916a9bbb81
KarinaKKarinaK <karinakalicka@gmail.com>
2025-07-13 01:16:26 +0400

    Updates for teh star background

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -1,4 +1,25 @@
+import { useState
+
+ } from "react";
 export const StarBackground = () => {
-    cont [Stars,setStars] = useState();
-    return <div></div>;
-}
+  cont[(Stars, setStars)] = useState();
+
+  const generateStars = () => {
+    const numberOfStars = Math.floor(
+        (window.innerWidth * window.innerHeight) / 10000
+    );
+
+    const newStars = [];
+
+    for (let i = 0; i < numberOfStars; i++){
+        newStars.push({
+            id: i,
+            size: Math.random() * 3 + 1,
+            x: Math.random() * 100,
+            y: Math.random *100,
+        })
+    }
+  };
+
+  return <div></div>;
+};
