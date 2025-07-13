217a462e1c87cc5d8b681b5ceb875599eaef75c2
KarinaKKarinaK <karinakalicka@gmail.com>
2025-07-13 23:06:25 +0800

    Addedd more navbar code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,6 +18,22 @@ export const Navbar = () => {
   }, []);
 
   return (
-    <nav className={cn("fixed w-full z-40 transition-all duration-300")}></nav>
+    <nav className={cn("fixed w-full z-40 transition-all duration-300",
+        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+    )}
+    >
+        <div className="container flex items-center justify-between">
+            <a>
+                <span>
+                    {" "}
+                    <span className="text-glow"> Karina</span> Portfolio
+                </span>
+            </a>
+
+        </div>
+
+
+
+    </nav>
   );
 };
