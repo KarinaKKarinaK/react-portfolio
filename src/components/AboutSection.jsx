3824a6573640b6ee0c7097dcdac082a352b20559
KarinaKKarinaK <karinakalicka@gmail.com>
2025-07-15 22:55:00 +0800

    Updated about me section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -2,8 +2,8 @@ export const AboutSection = () => {
     return <section id="about" className="py-24 px-4 relative"> 
         {" "}
         <div className="container mx-auto max-w-5xl">
-            <h2>
-                About <span></span>
+            <h2 className="text-3xl md:text-4xl font-bold mb-12">
+                About <span className="text-primary">Me</span>
             </h2>
         </div>
     </section>
