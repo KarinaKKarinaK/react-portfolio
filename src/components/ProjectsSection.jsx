6367d3836344ddc31778dcdb428f43960f2ee477
KarinaKKarinaK <karinakalicka@gmail.com>
2025-07-16 23:24:34 +0800

    Started projects section

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,28 @@
+const projects = [
+    {
+        title: "Project One",
+        description: "A brief description of Project One.",
+        image: "/path/to/image1.jpg",
+        link: "#"
+    },
+    {
+        title: "Project Two",
+        description: "A brief description of Project Two.",
+        image: "/path/to/image2.jpg",
+        link: "#"
+    },
+    {
+        title: "Project Three",
+        description: "A brief description of Project Three.",
+        image: "/path/to/image3.jpg",
+        link: "#"
+    }
+]
+
+export const ProjectsSection = () => {
+  return (
+    <section className="py-16 bg-background">
+    </section>
+  );
+};
+// This is a placeholder for the Projects section. You can replace the example project card with actual
