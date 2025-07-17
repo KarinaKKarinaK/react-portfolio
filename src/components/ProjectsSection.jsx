57819a39786aa13995868e521aec911fc0e47413
KarinaKKarinaK <karinakalicka@gmail.com>
2025-07-17 22:05:06 +0800

    Added updates to projects section

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,20 +1,24 @@
 const projects = [
-    {
+  {
     id: 1,
     title: "Chess With Stockfish Engine",
-    description: "A Python chess game with a GUI using Pygame and Stockfish engine integration.",
+    description:
+      "A Python chess game with a GUI using Pygame and Stockfish engine integration.",
     image: "/projects/project23.png",
     tags: ["Python", "Pygame", "Stockfish"],
-    demoUrl: "https://github.com/KarinaKKarinaK/ChessWithEngine/blob/main/assets/images/project23.png",
-    githubUrl: "https://github.com/KarinaKKarinaK/ChessWithEngine"
+    demoUrl:
+      "https://github.com/KarinaKKarinaK/ChessWithEngine/blob/main/assets/images/project23.png",
+    githubUrl: "https://github.com/KarinaKKarinaK/ChessWithEngine",
   },
   {
     id: 2,
     title: "Expense Tracker",
-    description: "A modern glassmorphism expense tracker built with HTML, CSS, and vanilla JS.",
+    description:
+      "A modern glassmorphism expense tracker built with HTML, CSS, and vanilla JS.",
     image: "/projects/project21.png",
     tags: ["HTML", "CSS", "JavaScript", "LocalStorage"],
-    demoUrl: "https://github.com/KarinaKKarinaK/expense-tracker/blob/main/screenshots/Screenshot%202025-06-26%20at%2017.32.05.png",
+    demoUrl:
+      "https://github.com/KarinaKKarinaK/expense-tracker/blob/main/screenshots/Screenshot%202025-06-26%20at%2017.32.05.png",
     githubUrl: "https://github.com/KarinaKKarinaK/expense-tracker",
   },
   {
@@ -23,29 +27,32 @@ const projects = [
     description: "Modern portfolio site with animations and dark mode.",
     image: "/projects/project4.png",
     tags: ["HTML, CSS", "JavaScript"],
-    demoUrl: "https://github.com/KarinaKKarinaK/react-portfolio?tab=readme-ov-file",
-    githubUrl: "https://github.com/KarinaKKarinaK/personal-portfolio/tree/main/portfolio",
-  },
-  {
-    id: 2,
-    title: "Interactive AI-Powered Spanish Learning App",
-    description: "A Spanish learning app built with Python, Streamlit, and OpenAI API. Includes login, chat, speech tools, and language AI helpers.",
-    image: "/projects/project24.png",
-    tags: ["Python", "Streamlit", "OpenAI", "SQLite", "gTTS"],
-    demoUrl: "https://github.com/KarinaKKarinaK/SpanishApp",
-    githubUrl: "https://github.com/KarinaKKarinaK/SpanishApp"
+    demoUrl:
+      "https://github.com/KarinaKKarinaK/react-portfolio?tab=readme-ov-file",
+    githubUrl:
+      "https://github.com/KarinaKKarinaK/personal-portfolio/tree/main/portfolio",
   },
   {
     id: 4,
+    title: "Interactive AI-Powered Spanish Learning App",
+    description:
+      "A Spanish learning app built with Python, Streamlit, and OpenAI API. Includes login, chat, speech tools, and language AI helpers.",
+    image: "/projects/project24.png",
+    tags: ["Python", "Streamlit", "OpenAI", "SQLite", "gTTS"],
+    demoUrl: "https://github.com/KarinaKKarinaK/SpanishApp",
+    githubUrl: "https://github.com/KarinaKKarinaK/SpanishApp",
+  },
+  {
+    id: 5,
     title: "Portfolio Website",
     description: "A hacker-inspired portfolio website with a unique design.",
     image: "/projects/project25.png",
-    tags: ["Vite", "TailwindCSS","JavaScript","HTML", "CSS"],
+    tags: ["Vite", "TailwindCSS", "JavaScript", "HTML", "CSS"],
     demoUrl: "#",
     githubUrl: "#",
   },
   {
-    id: 7,
+    id: 6,
     title: "SaaS Website",
     description: "Landing page with testimonials and animations.",
     image: "/projects/project13.png",
@@ -54,7 +61,7 @@ const projects = [
     githubUrl: "#",
   },
   {
-    id: 8,
+    id: 7,
     title: "Dashboard UI",
     description: "Analytics dashboard with graphs and responsive layout.",
     image: "/projects/project16.png",
@@ -63,29 +70,56 @@ const projects = [
     githubUrl: "#",
   },
   {
-    id: 3,
+    id: 8,
     title: "Quiz Game",
-    description: "Interactive quiz web app with instant feedback and progress tracking.",
+    description:
+      "Interactive quiz web app with instant feedback and progress tracking.",
     image: "/projects/project4.png",
     tags: ["JavaScript", "CSS", "HTML"],
-    demoUrl: "https://github.com/KarinaKKarinaK/quiz-game/blob/main/screenshots/Screenshot%202025-06-26%20at%200.30.09.png",
+    demoUrl:
+      "https://github.com/KarinaKKarinaK/quiz-game/blob/main/screenshots/Screenshot%202025-06-26%20at%200.30.09.png",
     githubUrl: "https://github.com/KarinaKKarinaK/quiz-game",
   },
   {
-    id: 10,
+    id: 9,
     title: "Digital Business Card",
     description: "Digital business card using dummy data and animations.",
     image: "/projects/project19.png",
     tags: ["HTML", "CSS"],
     demoUrl: "#",
-    githubUrl: "https://github.com/KarinaKKarinaK/html_business_card?tab=readme-ov-file",
+    githubUrl:
+      "https://github.com/KarinaKKarinaK/html_business_card?tab=readme-ov-file",
   },
 ];
-
 
 export const ProjectsSection = () => {
   return (
     <section className="py-16 bg-background">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          {" "}
+          Featured <span className="text-primary"> Projects</span>
+        </h2>
+
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Here are some of my recent projects. Each project is a unique solution
+          to a problem or a creative endeavor that I am passionate about.
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((projects, key) => (
+            <div
+              key={key}
+              className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+            >
+              <div className="h-48 overflow-hidden">
+                <img src={projects.image} alt={projects.title}></img>
+              </div>
+
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
